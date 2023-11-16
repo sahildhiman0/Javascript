c4c093034297861520909a4a741b94ed7abaf3da
sahil dhiman <you@example.com>
2023-11-16 14:22:47 +0530

    hoisting assignment

---
+++ Assignments/assignmentHoist.js
@@ -0,0 +1,53 @@
+
+// ------------------Answer 1-------------------------------------
+// console.log(addNumbers(2,3));
+
+// function addNumbers(num1,num2){
+    //     return num1 + num2
+    // }
+    
+
+
+
+// ------------------Answer 2-------------------------------------
+// console.log(multiplyNumbers(2,3))
+
+// let multiplyNumbers = function(a,b){
+//     return a * b;
+// }
+
+
+// ------------------Answer 3-------------------------------------
+// console.log(addNumbers(10,20))
+
+// function addNumbers(num1,num2){
+//        console.log(val);
+//        var val = 10
+//         return num1 + num2
+//     }
+
+// ------------------Answer 4-------------------------------------
+
+// { 
+    
+    // console.log(var1);
+    // console.log(var2);
+    // console.log(var3);
+
+    // let var1 = 1
+    // var var2 = 2
+    // const var3 = 3
+    
+    // console.log(var1);
+    // console.log(var2);
+    // console.log(var3);
+// }
+
+// ------------------Answer 5-------------------------------------
+
+// {
+    // console.log(var1);
+    // let var1 = 2
+    // console.log(var1);
+
+// }
